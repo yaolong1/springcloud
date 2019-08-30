5866315a005abe9a9fb2aa48982c6138b2c8fe1e
“yaolong <1638538651@qq.com>
2019-08-30 19:25:32 +0800

    zuul网关过滤和hystrix断路器（熔断、降级、监控）

---
+++ springcloud/microservicecloud-provider-dept-hystrix-8001/src/main/java/com/yaolong/springcloud/DeptProvider8001_Hystrix_App.java
@@ -0,0 +1,27 @@
+package com.yaolong.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author yaoLong
+ * @date 2019/8/25  14:14
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient//开启发现
+@EnableCircuitBreaker
+@MapperScan("com.yaolong.springcloud.dao")
+public class DeptProvider8001_Hystrix_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8001_Hystrix_App.class,args);
+    }
+
+}
---
+++ springcloud/microservicecloud-provider-dept-hystrix-8001/src/main/java/com/yaolong/springcloud/controller/DeptController.java
@@ -0,0 +1,33 @@
+package com.yaolong.springcloud.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.yaolong.springcloud.entities.Dept;
+import com.yaolong.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author yaoLong
+ * @date 2019/8/25  14:13
+ */
+@RestController
+public class DeptController {
+    @Autowired
+    private DeptService service = null;
+
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
+    public Dept get(@PathVariable("id") Long id) {
+        Dept dept = this.service.get(id);
+        if (null == dept) {
+            throw new RuntimeException("该ID：" + id + "没有没有对应的信息");
+        }
+        return dept;
+    }
+
+    public Dept processHystrix_Get(@PathVariable("id") Long id) {
+        return new Dept().setDeptno(id)
+                .setDname("该ID：" + id + "没有没有对应的信息,null--@HystrixCommand")
+                .setDb_source("no this database in MySQL");
+    }
+}
